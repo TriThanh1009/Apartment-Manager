2971aef70b5251429522595564a33330e6f41700
ThanhRua <92020142+Turtleeeeeee@users.noreply.github.com>
2023-06-26 21:59:22 +0700

    1

--- ApartmentManager/Data/Configurations/PaymentExtensionConfiguration.cs
+++ ApartmentManager/Data/Configurations/PaymentExtensionConfiguration.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Configurations
+namespace Data.Configurations
 {
     public class PaymentExtensionConfiguration : IEntityTypeConfiguration<PaymentExtension>
     {
