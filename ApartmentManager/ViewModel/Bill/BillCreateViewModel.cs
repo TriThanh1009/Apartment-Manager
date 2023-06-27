cff5f72ea8f92ffc013528a76654fc9cacfceeb8
ThanhRua <92020142+Turtleeeeeee@users.noreply.github.com>
2023-06-28 00:01:11 +0700

    1

--- ApartmentManager/ViewModel/Bill/BillCreateViewModel.cs
+++ ApartmentManager/ViewModel/Bill/BillCreateViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace ViewModel.Bill
 {
-    internal class BillCreateViewModel
+    public class BillCreateViewModel
     {
+        public int ID { get; set; }
+        public int IDRTC { get; set; }
+        public int ElectricQuantity { get; set; }
+        public Active Active { get; set; }
+        public DateTime PayDate { get; set; }
+        public int TotalMoney { get; set; }
     }
 }
