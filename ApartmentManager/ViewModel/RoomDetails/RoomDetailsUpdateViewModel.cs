08b7e2923ad9ac6983ecc2291a1f5bf6e7999300
ThanhRua <92020142+Turtleeeeeee@users.noreply.github.com>
2023-06-28 17:26:23 +0700

    1

---
+++ ApartmentManager/ViewModel/RoomDetails/RoomDetailsUpdateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.RoomDetails
+{
+    public class RoomDetailsUpdateViewModel
+    {
+        public string IDFur { get; set; }
+        public string IDRoom { get; set; }
+    }
+}
