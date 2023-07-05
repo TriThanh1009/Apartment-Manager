13690e75e1e094f316c5ff148bd234f772c5d23f
thienlhtst <137595843+T2Pro@users.noreply.github.com>
2023-07-05 23:56:07 +0700

    up home data

--- ApartmentManager/UI/View/Homes/Home.xaml.cs
+++ ApartmentManager/UI/View/Homes/Home.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,10 +25,30 @@ namespace UI.View.Homes
         public Home()
         {
             InitializeComponent();
+            ObservableCollection<Nemmber> nember = new ObservableCollection<Nemmber>();
+            nember.Add(new Nemmber() { ID =1, Charactic="T", Name="Thien", Old="2", Year ="1" });
+            nember.Add(new Nemmber() { ID =2, Charactic="T", Name="Thien", Old="2", Year ="2" });
+            nember.Add(new Nemmber() { ID =3, Charactic="T", Name="Thien", Old="2", Year ="3" });
+            nember.Add(new Nemmber() { ID =4, Charactic="T", Name="Thien", Old="2", Year ="4" });
+            nember.Add(new Nemmber() { ID =5, Charactic="T", Name="Thien", Old="2", Year ="5" });
+            nember.Add(new Nemmber() { ID =6, Charactic="T", Name="Thien", Old="2", Year ="6" });
+            nember.Add(new Nemmber() { ID =7, Charactic="T", Name="Thien", Old="2", Year ="7" });
+            nember.Add(new Nemmber() { ID =8, Charactic="T", Name="Thien", Old="2", Year ="8" });
+
+            homedata.ItemsSource=nember;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
         }
+    }
+
+    public class Nemmber
+    {
+        public int ID { get; set; }
+        public string Charactic { get; set; }
+        public string Name { get; set; }
+        public string Old { get; set; }
+        public string Year { get; set; }
     }
 }
