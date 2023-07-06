665f633ceb8df6c86e0108467fdd0d51215ac8d0
thienlhtst <137595843+T2Pro@users.noreply.github.com>
2023-07-07 01:07:27 +0700

    up

--- ApartmentManager/UI/obj/Debug/net7.0-windows/App.g.i.cs
+++ ApartmentManager/UI/obj/Debug/net7.0-windows/App.g.i.cs
@@ -62,7 +62,7 @@ namespace UI {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/AM.UI;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AM.UI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- ApartmentManager/UI/obj/Debug/net7.0-windows/View/RentalContract/RentalContactDelete.g.i.cs
+++ ApartmentManager/UI/obj/Debug/net7.0-windows/View/RentalContract/RentalContactDelete.g.i.cs
@@ -53,7 +53,7 @@ namespace AM.UI.View.RentalContract {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AM.UI;component/view/rentalcontract/rentalcontactdelete.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AM.UI;V1.0.0.0;component/view/rentalcontract/rentalcontactdelete.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\RentalContract\RentalContactDelete.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
