8b71f1263c15b65902a1ba0b44ddf07d6fda2aae
thienlhtst <137595843+T2Pro@users.noreply.github.com>
2023-07-08 00:11:07 +0700

    up

--- ApartmentManager/Services/Interface/IPeople.cs
+++ ApartmentManager/Services/Interface/IPeople.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModel.Dtos;
+using ViewModel.People;
 
 namespace Services.Interface
 {
     public interface IPeople
     {
+        Task<PagedResult<CustomerVM>> GetAllPage(RequestPaging request);
     }
 }
---
+++ ApartmentManager/UI/Utilities/Regex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.UI.Utilities
+{
+    public class Regex
+    {
+        public Regex()
+        { }
+
+        public int Toint(string a)
+        {
+            return Convert.ToInt32(a);
+        }
+    }
+}
