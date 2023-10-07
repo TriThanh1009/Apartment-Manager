9612dc3e4a3ba602c867e6acd5acfbf816d61fad
thienlhtst <137595843+T2Pro@users.noreply.github.com>
2023-10-07 20:15:10 +0700

    up

--- ApartmentManager/UI/obj/Debug/net7.0-windows/View/RentalContract/RentalContractAdd.g.i.cs
+++ ApartmentManager/UI/obj/Debug/net7.0-windows/View/RentalContract/RentalContractAdd.g.i.cs
@@ -53,7 +53,7 @@ namespace AM.UI.View.RentalContract {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AM.UI;component/view/rentalcontract/rentalcontractadd.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AM.UI;V1.0.0.0;component/view/rentalcontract/rentalcontractadd.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\RentalContract\RentalContractAdd.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
